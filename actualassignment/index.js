4d1ebf6d904104b1547e4b6ee001c89846659f3d
Andreea <andreea@Micleas-MacBook.local>
2018-11-06 19:19:21 +0100

    commit glow

--- actualassignment/index.js
+++ actualassignment/index.js
@@ -1,13 +1,24 @@
-const dog = document.document.querySelector('#bg');
-const owner = document.document.querySelector('#dog');
+const dog = document.querySelector('#bg');
+const owner = document.querySelector('#dog');
 const bone = document.querySelector('#bone');
 const time = document.querySelector('#time')
 
+
+let btnglow = document.querySelector("#btnglow");
+btnglow.addEventListener("click",glow);
+
+function glow () {
+    time.classList.add("glow");
+}
+
 time.addEventListener('click',  shiftt);
 dog.addEventListener('mouseover', bark);
-owner.addEventListener('click', throw);
+owner.addEventListener('click', throw1);
+//throw is a key word
 
-function throw() {
+function throw1() {
   dog.addEventListener('click', run);
   bone.style.display = 'block';
 }
+
+

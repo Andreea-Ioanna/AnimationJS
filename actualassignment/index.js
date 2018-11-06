5228a2c4ef6d34a5f05ed46bd78af5527d775f75
Valentin-Florin <Valentin99dumitrache@gmail.com>
2018-11-06 19:13:56 +0100

    ma duc la sala l-am facut sa latre si am facut si sa apara osu cand apesi pe om

--- actualassignment/index.js
+++ actualassignment/index.js
@@ -1,13 +1,19 @@
-const dog = document.document.querySelector('#bg');
-const owner = document.document.querySelector('#dog');
-const bone = document.querySelector('#bone');
-const time = document.querySelector('#time')
+const dog = document.querySelector("#dog");
+const owner = document.querySelector("#owner");
+const bone = document.querySelector("#bone");
+const time = document.querySelector("#time");
+const barkk = document.querySelector('#bark')
 
-time.addEventListener('click',  shiftt);
-dog.addEventListener('mouseover', bark);
-owner.addEventListener('click', throw);
+// time.addEventListener('click',  shiftt);
+dog.addEventListener("mouseover", bark);
+owner.addEventListener("click", throww);
 
-function throw() {
-  dog.addEventListener('click', run);
+function throww() {
+  // dog.addEventListener('click', run);
   bone.style.display = 'block';
+
+}
+function bark() {
+  dog.classList.add('bark');
+  barkk.play();
 }
